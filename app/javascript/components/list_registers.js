078fb3f6926738800246c8b46b571db5e8b2edb3
danielcapera <caperajn@gmail.com>
2021-02-16 09:00:42 -0500

    create scaffold of people registers

---
+++ app/javascript/components/list_registers.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom'
+
+class ListRegisters extends React.Component {
+  render() {
+    return (
+      <h1> Hello React </h1>
+    )
+  }
+}
+
+export default ListRegisters;
